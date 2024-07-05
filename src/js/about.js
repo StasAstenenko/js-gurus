e3bde53891f62f477274d9e355c00f1ca61fb590
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-07-06 01:21:36 +0200

    create accordion style from about, add html and style swipper from about

---
+++ src/js/about.js
@@ -0,0 +1,8 @@
+// import Accordion from 'accordion-js';
+
+// const accordions = Array.from(document.querySelector('.js-ac-about'));
+
+// new Accordion('.js-ac-about', { showMultiple: true });
+
+// const list = document.querySelector('.js-ac-about');
+// console.log(list);
