3b3d1aceeb604026d389500fde4fd7ec8c1a2ee8
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-07-05 22:46:56 +0200

    create accordion style in about style file

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,4 +1,4 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
- new Accordion('.faq-list', {showMultiple: true,});
+new Accordion('.faq-list', { showMultiple: true });
