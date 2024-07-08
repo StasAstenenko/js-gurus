5def830802aaec18923fb404452340035c2b8050
VictoriaMendas <124498217+VictoriaMendas@users.noreply.github.com>
2024-07-08 17:38:57 +0200

    main work done

--- src/js/header.js
+++ src/js/header.js
@@ -1,13 +1,23 @@
 const headerList = document.querySelector('.header-list');
 const headerMenu = document.querySelector('.header-menu');
+const headerContainer = document.querySelector('.header-menu-container');
+const headerLink = document.querySelector('.header-link');
 
 headerMenu.addEventListener('click', e => {
-  const list = e.currentTarget;
-  console.log(list);
-  console.log(headerList);
-  if (!e.target) {
-    return;
-  } else {
-    headerList.classList.remove('display: none');
+  e.preventDefault();
+
+  headerList.classList.toggle('dropdown-menu');
+});
+
+headerLink.addEventListener('click', e => {
+  headerList.classList.remove('dropdown-menu');
+});
+
+document.addEventListener('click', function (e) {
+  if (
+    !headerContainer.contains(e.target) &&
+    headerList.classList.contains('dropdown-menu')
+  ) {
+    headerList.classList.remove('dropdown-menu');
   }
 });
