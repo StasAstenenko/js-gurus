2bcad01e08c4ad6cb567a83524c406d35d26d60d
Anya2907 <67818359+Anya2907@users.noreply.github.com>
2024-07-09 09:34:10 +0300

    footer fine

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,41 +1,44 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // const modal1 = document.getElementById('modal1');
+
+    const modal1 = document.getElementById('modal1');
     const modal2 = document.getElementById('modal2');
     // const openModal1Btn = document.getElementById('openModal1Btn');
     const openModal2Btn = document.getElementById('burger-icon');
-    // const closeModal1Btn = document.getElementById('closeModal1Btn');
+    const closeModal1Btn = document.getElementById('closeModal1Btn');
     const closeModal2Btn = document.getElementById('closeModal2Btn');
     const navLinks = document.querySelectorAll('.nav-link, .benefits-link');
+    const body = document.querySelector('body')
 
-    // const openModal1 = () => {
-    //     modal1.style.display = 'flex';
-    //     setTimeout(() => modal1.style.opacity = '1', 10); 
-    // };
+    const openModal1 = () => {
+        modal1.style.display = 'flex';
+        setTimeout(() => modal1.style.opacity = '1', 10); 
+    };
 
-    // const closeModal1 = () => {
-    //     modal1.style.opacity = '0';
-    //     setTimeout(() => modal1.style.display = 'none', 250);  
-    // };
+    const closeModal1 = () => {
+        modal1.style.opacity = '0';
+        setTimeout(() => modal1.style.display = 'none', 250);  
+    };
 
     const openModal2 = () => {
         modal2.style.display = 'flex';
+        body.style.overflow = 'hidden';
         setTimeout(() => modal2.style.opacity = '1', 10); 
     };
 
     const closeModal2 = () => {
         modal2.style.opacity = '0';
+        body.style.overflow = 'scroll';
         setTimeout(() => modal2.style.display = 'none', 250);  
     };
 
-    // openModal1Btn.addEventListener('click', openModal1);
-    // closeModal1Btn.addEventListener('click', closeModal1);
+    // openModal1Btn.addEventListener('submit', openModal1);
+    closeModal1Btn.addEventListener('click', closeModal1);
     openModal2Btn.addEventListener('click', openModal2);
     closeModal2Btn.addEventListener('click', closeModal2);
 
     window.addEventListener('click', (event) => {
-        // if (event.target === modal1) {
-        //     closeModal1();
-        // }
+        if (event.target === modal1) {
+            closeModal1();
+        }
         if (event.target === modal2) {
             closeModal2();
         }
@@ -43,7 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('keydown', (event) => {
         if (event.key === 'Escape') {
-            // closeModal1();
+            closeModal1();
             closeModal2();
         }
     });
@@ -63,4 +66,5 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-});
+
+export default openModal1;
